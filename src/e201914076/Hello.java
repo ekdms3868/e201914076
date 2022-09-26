5f95e76deebced4a9649bdedaae8198b9d77b369
daeun <ehsjt3868@naver.com>
2022-09-26 16:46:00 +0900

    Main.java 수정 #3, Hello.java 수정 #3

--- src/e201914076/Hello.java
+++ src/e201914076/Hello.java
@@ -3,6 +3,10 @@ package e201914076;
 public class Hello {
     String msg = "hello";
 
+    public Hello(String msg) {
+        this.msg = msg;
+    }
+
     public void say() {
         System.out.println(msg);
     }
--- src/e201914076/Main.java
+++ src/e201914076/Main.java
@@ -2,9 +2,8 @@ package e201914076;
 
 public class Main {
     
-        public static void main(String[] args) {
-        Hello hello = new Hello();
+    public static void main(String[] args) {
+        Hello hello = new Hello("hello world");
         hello.say();
     }
-
 }
