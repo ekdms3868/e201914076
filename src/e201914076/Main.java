84d6212c0f531646b204a39f5a54ba8226c83212
daeun <ehsjt3868@naver.com>
2022-09-26 16:47:47 +0900

    Main.java 수정 #4

--- src/e201914076/Main.java
+++ src/e201914076/Main.java
@@ -1,8 +1,13 @@
 package e201914076;
 
 public class Main {
+
+    static void say() {
+        System.out.println("main");
+    }
     
     public static void main(String[] args) {
+        say();
         Hello hello = new Hello();
         hello.say("hello world");
     }
